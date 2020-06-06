51d0f6905d318abbb1b527f586c730bdba6bfbdf
Victor M. Brito <hdbowser@gmail.com>
2020-06-05 21:09:33 -0400

    Carga inicial de datos

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+  import Home from "./Pages/Home.svelte";
+</script>
+
+<Home />
---
+++ src/Layout/Aside.svelte
@@ -0,0 +1,84 @@
+<aside class="admin-sidebar">
+  <div class="admin-sidebar-brand">
+    <!-- begin sidebar branding-->
+    <img
+      class="admin-brand-logo"
+      src="assets/img/logo.svg"
+      width="40"
+      alt="atmos Logo" />
+    <span class="admin-brand-content">
+      <a href="index.html">atmos</a>
+    </span>
+    <!-- end sidebar branding-->
+    <div class="ml-auto">
+      <!-- sidebar pin-->
+      <a href="#!" class="admin-pin-sidebar btn-ghost btn btn-rounded-circle" />
+      <!-- sidebar close for mobile device-->
+      <a href="#!" class="admin-close-sidebar" />
+    </div>
+  </div>
+  <div class="admin-sidebar-wrapper js-scrollbar">
+    <!-- Menu List Begins-->
+    <ul class="menu">
+      <!--list item begins-->
+      <li class="menu-item active">
+        <a href="#!" class="menu-link">
+          <span class="menu-label">
+            <span class="menu-name">Escritorio</span>
+          </span>
+          <span class="menu-icon">
+            <span class="icon-badge badge-success badge badge-pill">1</span>
+
+            <i class="icon-placeholder mdi mdi-link-variant " />
+          </span>
+        </a>
+
+      </li>
+      <!--list item ends-->
+
+      <!--list item begins-->
+      <li class="menu-item ">
+        <a href="#!" class="open-dropdown menu-link">
+          <span class="menu-label">
+            <span class="menu-name">
+              Mantenimiento
+              <span class="menu-arrow" />
+            </span>
+            <span class="menu-info">Contains submenu</span>
+          </span>
+          <span class="menu-icon">
+            <i class="icon-placeholder mdi mdi-link-variant " />
+          </span>
+        </a>
+        <!--submenu-->
+        <ul class="sub-menu">
+          <li class="menu-item">
+            <a href="#!" class=" menu-link">
+              <span class="menu-label">
+                <span class="menu-name">Usuarios</span>
+              </span>
+              <span class="menu-icon">
+                <i class="icon-placeholder ">U</i>
+              </span>
+            </a>
+
+          </li>
+          <li class="menu-item">
+            <a href="#!" class=" menu-link">
+              <span class="menu-label">
+                <span class="menu-name">Empresa</span>
+              </span>
+              <span class="menu-icon">
+                <i class="icon-placeholder ">E</i>
+              </span>
+            </a>
+
+          </li>
+
+        </ul>
+      </li>
+    </ul>
+    <!-- Menu List Ends-->
+  </div>
+
+</aside>
